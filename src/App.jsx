a587447fb21c1134c0eea612c009d0ca9766904a
stautuan <stautuan89@gmail.com>
2024-05-12 09:27:53 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Shootout from './Shootout';
+
+function App() {
+    return (
+        <main className="game-container">
+            <Shootout />
+        </main>
+    );
+}
+
+export default App;
---
+++ src/Shootout.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+
+function Shootout() {
+    const [count, setCount] = useState(0);
+    const [displayAllCount, setDisplayAllCount] = useState([]);
+    const [message, setMessage] = useState('');
+    const [condition, setCondition] = useState('');
+
+    // Count the paces
+    useEffect(() => {
+        // Renders the pace every second from 1 to 10
+        const paces = setTimeout(() => {
+            if (count < 10) {
+                setCount(count + 1);
+                setDisplayAllCount((counts) => [...counts, count + 1]);
+            } else {
+                // Generate a delay before rendering a signal to press a key
+                const randomDelay = (Math.floor(Math.random() * 5) + 1) * 1000;
+                setTimeout(() => {
+                    setMessage('He draws.....');
+
+                    const defeat = setTimeout(() => {
+                        setCondition('And shoots. You are dead.');
+                        window.removeEventListener('keydown', victory);
+                    }, 300);
+
+                    function victory() {
+                        clearTimeout(defeat);
+                        setCondition('But you shoot first. You killed him.');
+                    }
+                    window.addEventListener('keydown', victory);
+                }, randomDelay);
+            }
+        }, 1000);
+        return () => clearTimeout(paces);
+    }, [count]);
+
+    return (
+        <div>
+            <h1>Cowboy Shootout</h1>
+            <p>
+                You are back to back
+                <br />
+                Take 10 paces...
+                <br />
+            </p>
+            <p>
+                {displayAllCount.map((count) => (
+                    <p>{count}...</p>
+                ))}
+            </p>
+            <p>{message}</p>
+            <p>{condition}</p>
+        </div>
+    );
+}
+
+export default Shootout;
